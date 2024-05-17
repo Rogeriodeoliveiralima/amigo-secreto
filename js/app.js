d54a10cf6071157790f39b01ad9333d4b587fe85
Rogerio Lima <rlimapg@gmail.com>
2024-05-17 16:44:27 -0300

    estudando amigo secreto

---
+++ js/app.js
@@ -0,0 +1,60 @@
+let amigos = [];
+
+function adicionar(){
+    let amigo = document.getElementById('nome-amigo');
+    let listaAmigos = document.getElementById('lista-amigos');
+
+    amigos.push(amigo.value);
+
+    if(listaAmigos.textContent == '') {
+        listaAmigos.textContent = amigo.value;
+    }else{
+        listaAmigos.textContent =  listaAmigos.textContent + ', '+ amigo.value;
+    }
+
+    amigo.value ='';
+}
+
+
+
+function sortear(){
+
+    if(amigos.length < 4){
+         return alert('Adicione minimo de quatro amigos!');
+    }
+    
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for (let i = 0; i < amigos.length; i++){
+        if(i == amigos.length - 1){
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
+        }else{
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+        }
+       
+    }
+
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        
+        // guarda de um índice aleatório da lista
+        const elemento = lista[indice - 1];
+        
+        lista[indice - 1] = lista[indiceAleatorio];
+        
+        lista[indiceAleatorio] = elemento;
+    }
+}
+
+function reiniciar(){
+    amigos = [];
+    document.getElementById('nome-amigo').innerHTML = '';
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
