1ad842f64ac4303a2eabbfbdb89ee345b604e134
Rogerio Lima <rlimapg@gmail.com>
2024-05-20 16:29:44 -0300

    refazendo amigo segreto

--- js/app.js
+++ js/app.js
@@ -1,123 +1,68 @@
 let amigos = [];
 
+
 function adicionar(){
-    let amigo = document.getElementById('nome-amigo');
-    let listaAmigos = document.getElementById('lista-amigos');
 
-    amigos.push(amigo.value);
+  let nomeAmigos = document.getElementById('nome-amigo');
 
-    if(listaAmigos.textContent == '') {
-        listaAmigos.textContent = amigo.value;
-    }else{
-        listaAmigos.textContent =  listaAmigos.textContent + ', '+ amigo.value;
-    }
+  if(nomeAmigos.value == ''){
+   return alert('Informe nome de um amigo !!!!');
+  }
 
-    amigo.value ='';
+  if(amigos.includes(nomeAmigos.value)){
+    return alert('Amigos Ja adcionado!!');
+  }
+
+  let lista = document.getElementById('lista-amigos');
+  amigos.push(nomeAmigos.value);
+
+  if(lista.textContent == '' ){
+    lista.textContent = nomeAmigos.value;
+  }else {
+    lista.textContent = lista.textContent + ', '+ nomeAmigos.value;
+ }
+  nomeAmigos.value = '';
 }
-
 
 
 function sortear(){
+  if(amigos.length < 4){
+    return alert('Adicione no minimo quatro amigos!!')
+  }
 
-    if(amigos.length < 4){
-         return alert('Adicione minimo de quatro amigos!');
-    }
-    
-    embaralha(amigos);
-    let sorteio = document.getElementById('lista-sorteio');
+  embaralha(amigos);
 
-    for (let i = 0; i < amigos.length; i++){
-        if(i == amigos.length - 1){
-            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
-        }else{
-            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
-        }
-       
-    }
+  let sorteio = document.getElementById('lista-sorteio');
 
-}
-
-function embaralha(lista) {
-
-    for (let indice = lista.length; indice; indice--) {
-
-        const indiceAleatorio = Math.floor(Math.random() * indice);
-        
-        // guarda de um índice aleatório da lista
-        const elemento = lista[indice - 1];
-        
-        lista[indice - 1] = lista[indiceAleatorio];
-        
-        lista[indiceAleatorio] = elemento;
-    }
-}
-
-function reiniciar(){
-    amigos = [];
-    document.getElementById('nome-amigo').innerHTML = '';
-    document.getElementById('lista-amigos').innerHTML = '';
-    document.getElementById('lista-sorteio').innerHTML = '';
-}
-
-//  refazer exercicios
-
-let amigos = [];
-
-function adicionar(){
-    let amigo = document.getElementById('nome-amigo');
-    let listaAmigos = document.getElementById('lista-amigos');
-
-    amigos.push(amigo.value);
-
-    if(listaAmigos.textContent == '') {
-        listaAmigos.textContent = amigo.value;
+  for (let i = 0; i < amigos.length; i++){
+    if(i == amigos.length - 1){
+      sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> '+ amigos[0] + '<br>';
     }else{
-        listaAmigos.textContent =  listaAmigos.textContent + ', '+ amigo.value;
+      sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' -->'+ amigos[i + 1] + '<br>';
     }
+  }
 
-    amigo.value ='';
+}
+
+ function reiniciar(){
+  document.getElementById('lista-amigos').innerHTML = '';
+  document.getElementById('lista-sorteio').innerHTML = '';
+  amigos = [];
 }
 
 
-
-function sortear(){
-
-    if(amigos.length < 4){
-         return alert('Adicione minimo de quatro amigos!');
-    }
-    
-    embaralha(amigos);
-    let sorteio = document.getElementById('lista-sorteio');
-
-    for (let i = 0; i < amigos.length; i++){
-        if(i == amigos.length - 1){
-            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
-        }else{
-            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
-        }
-       
-    }
-
-}
 
 function embaralha(lista) {
 
-    for (let indice = lista.length; indice; indice--) {
+  for (let indice = lista.length; indice; indice--) {
 
-        const indiceAleatorio = Math.floor(Math.random() * indice);
-        
-        // guarda de um índice aleatório da lista
-        const elemento = lista[indice - 1];
-        
-        lista[indice - 1] = lista[indiceAleatorio];
-        
-        lista[indiceAleatorio] = elemento;
-    }
-}
-
-function reiniciar(){
-    amigos = [];
-    document.getElementById('nome-amigo').innerHTML = '';
-    document.getElementById('lista-amigos').innerHTML = '';
-    document.getElementById('lista-sorteio').innerHTML = '';
+      const indiceAleatorio = Math.floor(Math.random() * indice);
+      
+      // guarda de um índice aleatório da lista
+      const elemento = lista[indice - 1];
+      
+      lista[indice - 1] = lista[indiceAleatorio];
+      
+      lista[indiceAleatorio] = elemento;
+  }
 }
