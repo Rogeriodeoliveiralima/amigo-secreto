8589fa26a6d7a54aa59794ad0ccc5aa2489e3055
Rogerio Lima <rlimapg@gmail.com>
2024-05-24 18:10:10 -0300

    aula  3

--- js/app.js
+++ js/app.js
@@ -1,62 +1,64 @@
-
-let amigos = [];
+listaAmigos = [];
 
 function adicionar(){
   let nomeAmigo = document.getElementById('nome-amigo');
+
   if(nomeAmigo.value == ''){
-    return alert('Informe nome do amigo!')
+    return alert('Inclua o nome de um amigo!!');
+  }
+  
+  if(listaAmigos.includes(nomeAmigo.value)){
+    return alert('Esse nome ja esta lista de Amigos!');
   }
 
-  if(amigos.includes(nomeAmigo.value)){
-    return alert('Nome ja foi colocado na lista');
-  }
 
 
   let lista = document.getElementById('lista-amigos');
-  amigos.push(nomeAmigo.value);
+  listaAmigos.push(nomeAmigo.value);
 
   if(lista.textContent == ''){
     lista.textContent = nomeAmigo.value;
   }else{
-    lista.textContent = lista.textContent + ', '+ nomeAmigo.value;
+    lista.textContent = lista.textContent + (', ')+ nomeAmigo.value;
   }
+  
   nomeAmigo.value = '';
-
 }
 
 function sortear(){
-  if(amigos.length < 4){
-    return alert('Minimo de quatro amigos!');
+
+  if(listaAmigos.length < 4){
+    return alert('Minimo de quatro amigos para sorteio');
   }
 
-  embaralha(amigos);
+  embaralha(listaAmigos);
 
   let sorteio = document.getElementById('lista-sorteio');
-  for(let i = 0; i < amigos.length; i++){
-    if(i == amigos.length -1){
-      sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] +'<br>';
+
+  for (let i = 0; i < listaAmigos.length; i++){
+    if(i == listaAmigos.length -1){
+      sorteio.innerHTML = sorteio.innerHTML + listaAmigos[i] + ' --> ' + listaAmigos[0] + '<br>';
     }else{
-      sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] +'<br>';
+      sorteio.innerHTML = sorteio.innerHTML + listaAmigos[i] + ' --> ' + listaAmigos[i + 1] + '<br>';
     }
   }
 }
 
 function reiniciar(){
-  document.getElementById('nome-amigo').innerHTML = '';
-  document.getElementById('lista-amigos').innerHTML = '';
+  document.getElementById('lista-amigos').innerHTML = ''; 
   document.getElementById('lista-sorteio').innerHTML = '';
-  amigos = [];
 }
 
 
 function embaralha(lista) {
 
-  let indice = lista.length
-  
-  while(indice) {
-      // atenção para o pós-incremento indice-- 
-      const indiceAleatorio = Math.floor(Math.random() * indice--);
-      [lista[indice], lista[indiceAleatorio]] = 
-          [lista[indiceAleatorio], lista[indice]];
+  for (let indice = lista.length; indice; indice--) {
+
+      const indiceAleatorio = Math.floor(Math.random() * indice);
+
+      // atribuição via destructuring
+      [lista[indice - 1], lista[indiceAleatorio]] = 
+          [lista[indiceAleatorio], lista[indice - 1]];
   }
 }
+
